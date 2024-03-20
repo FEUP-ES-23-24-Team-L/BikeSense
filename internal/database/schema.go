520e77cd7c5128b96e6059cc6f15a5ad49ad7ddb
dvalnn <tiago.andre.amorim@gmail.com>
2024-03-20 16:41:15 +0000

    fixed json tags and removed gorm.model in favor of just using an ID field

--- internal/database/schema.go
+++ internal/database/schema.go
@@ -2,39 +2,37 @@ package database
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Bike struct {
-	gorm.Model `json:"skipWhenMarshal"`
-	Code       string `json:"code"`
-	trip       Trip
+	Code string `json:"code"`
+	trip Trip   `json:"-"`
+	ID   uint   `json:"id" gorm:"primaryKey"`
 }
 
 type SensorUnit struct {
-	gorm.Model `json:"skipWhenMarshal"`
-	Code       string `json:"code"`
-	Trip       Trip
+	Code string `json:"code"`
+	Trip Trip   `json:"-"`
+	ID   uint   `json:"id" gorm:"primaryKey"`
 }
 
 type Trip struct {
-	gorm.Model
 	DataPoint    []DataPoint `json:"-"`
+	ID           uint        `json:"id" gorm:"primaryKey"`
 	BikeID       uint        `json:"bike_id"`
 	SensorUnitID uint        `json:"sensor_unit_id"`
 }
 
 type DataPoint struct {
 	Timestamp            time.Time `json:"timestamp"`
-	gorm.Model           `json:"-"`
-	TripID               uint    `json:"trip_id"`
-	Location             float32 `json:"location"`
-	NoiseLevel           float32 `json:"noise_level"`
-	Temperature          float32 `json:"temperature"`
-	Humidity             float32 `json:"humidity"`
-	UVLevel              float32 `json:"uv_level"`
-	Luminosity           float32 `json:"luminosity"`
-	CarbonMonoxideLevel  float32 `json:"carbon_monoxide_level"`
-	PolutionParticlesPPM int32   `json:"polution_particles_ppm"`
+	ID                   uint      `json:"id" gorm:"primaryKey"`
+	TripID               uint      `json:"trip_id"`
+	Location             float32   `json:"location"`
+	NoiseLevel           float32   `json:"noise_level"`
+	Temperature          float32   `json:"temperature"`
+	Humidity             float32   `json:"humidity"`
+	UVLevel              float32   `json:"uv_level"`
+	Luminosity           float32   `json:"luminosity"`
+	CarbonMonoxideLevel  float32   `json:"carbon_monoxide_level"`
+	PolutionParticlesPPM int32     `json:"polution_particles_ppm"`
 }
