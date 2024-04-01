120106c97f9f3eea2f9d94cb8ed3378e85cc7805
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-04-01 22:31:08 +0100

    mock dataset and upload script

--- scripts/upload_mock_data.sh
+++ scripts/upload_mock_data.sh
@@ -8,4 +8,6 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 export PGPASSWORD="${DB_PASSWORD}"
-psql -U "${DB_USER}" -h "${DB_HOST}" -p "${DB_PORT}" -d "${DB_NAME}"
+echo "Uploading mock data to the database..."
+psql -U "${DB_USER}" -h "${DB_HOST}" -p "${DB_PORT}" -d "${DB_NAME}" -f mock_data.sql >/dev/null
+echo "Mock data has been uploaded to the database!"
